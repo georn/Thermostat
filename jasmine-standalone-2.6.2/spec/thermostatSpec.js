1ddd247eeebb7d492f0d63836d4b9b384b1029e0
Spencer <spencer.bartonfisher@me.com>
2017-06-07 12:16:58 +0100

    added an up function

--- jasmine-standalone-2.6.2/spec/thermostatSpec.js
+++ jasmine-standalone-2.6.2/spec/thermostatSpec.js
@@ -5,9 +5,17 @@ describe ('thermostat', function() {
   beforeEach(function() {
     thermostat = new Thermostat;
   });
+
   describe('default temperature', function() {
     it('shows a defualt temperature of 20', function() {
       expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('#up', function() {
+    it('increases temperature by 1', function() {
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21);
     });
   });
 });
--- jasmine-standalone-2.6.2/src/thermostat.js
+++ jasmine-standalone-2.6.2/src/thermostat.js
@@ -1,4 +1,8 @@
 var Thermostat = function() {
   var DEFAULT_TEMPERATURE = 20
   this.temperature = DEFAULT_TEMPERATURE;
+
+  this.up = function() {
+    this.temperature++;
+  };
 };
